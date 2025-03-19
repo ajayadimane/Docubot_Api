2df0c4e80226c028fe264d7d86f5e191740f3bc7
Ajay <ajayadimane@botaiml.com>
2025-03-19 18:23:56 +0530

    Context apth and val chnages

---
+++ Models/Doqbot_Models/Kval.cs
@@ -0,0 +1,20 @@
+﻿namespace DocuBot_Api.Models.Doqbot_Models
+{
+        public class Kval
+        {
+            public int Id { get; set; }
+            public int Lfid { get; set; }
+            public string Linedump { get; set; }
+            public string K1 { get; set; }
+            public string Kval1 { get; set; }
+            public string K2 { get; set; }
+            public string Kval2 { get; set; }
+            public string K3 { get; set; }
+            public string Kval3 { get; set; }
+            public string K4 { get; set; }
+            public string Kval4 { get; set; }
+            public string Locn { get; set; }
+        }
+
+    
+}
---
+++ Models/Doqbot_Models/StoredProcedureValues.cs
@@ -0,0 +1,8 @@
+﻿namespace DocuBot_Api.Models.Doqbot_Models
+{
+    public class StoredProcedureValues
+    {
+        public string path { get; set; }
+        public string usr { get; set; }
+    }
+}
